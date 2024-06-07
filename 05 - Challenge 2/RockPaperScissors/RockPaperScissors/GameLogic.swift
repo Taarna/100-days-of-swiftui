ba7f03c59e5d0aab52ba91ac9e613fcedf60b18c
Taarna <ivana.rast@gmail.com>
2024-06-07 13:11:37 +0200

    Cleanup

--- 05 - Challenge 2/RockPaperScissors/RockPaperScissors/GameLogic.swift
+++ 05 - Challenge 2/RockPaperScissors/RockPaperScissors/GameLogic.swift
@@ -8,22 +8,44 @@ enum Move: String, CaseIterable {
 
 @Observable
 class GameLogic {
-    private(set) var playerShouldWin: Bool = Bool.random()
-    private(set) var score: Int
     private(set) var currentMove: Move
+    private(set) var playerShouldWin: Bool = Bool.random()
+    
+    private(set) var score: Int
+    
     private(set) var remainingAttempts: Int
-    private(set) var maxAttempts = Constants.maxAttempts
+    let maxAttempts = Constants.maxAttempts
+    
     private(set) var isGameFinished = false
     
     init() {
-        playerShouldWin = Bool.random()
-        score = 0
         currentMove = Constants.possibleMoves.randomElement()!
+        playerShouldWin = Bool.random()
+        
+        score = 0
+        
         remainingAttempts = 10
+        
         isGameFinished = false
     }
     
-    func playerSelected(move: Move){
+    func startNewGame() {
+        currentMove = Constants.possibleMoves.randomElement()!
+        playerShouldWin = Bool.random()
+        
+        score = 0
+        
+        remainingAttempts = 10
+        
+        isGameFinished = false
+    }
+    
+    func startNewTurn() {
+        currentMove = Constants.possibleMoves.randomElement()!
+        playerShouldWin = Bool.random()
+    }
+    
+    func playerSelected(move: Move) {
         let isMoveWinning = isMoveWinning(move: move)
         let isAnswerCorrect = isMoveWinning && playerShouldWin || !isMoveWinning && !playerShouldWin
         
@@ -40,19 +62,6 @@ class GameLogic {
         if !isGameFinished {
             startNewTurn()
         }
-    }
-    
-    func startNewGame() {
-        playerShouldWin = Bool.random()
-        score = 0
-        currentMove = Constants.possibleMoves.randomElement()!
-        remainingAttempts = 10
-        isGameFinished = false
-    }
-    
-    func startNewTurn() {
-        playerShouldWin = Bool.random()
-        currentMove = Constants.possibleMoves.randomElement()!
     }
     
     private func isMoveWinning(move: Move) -> Bool {
