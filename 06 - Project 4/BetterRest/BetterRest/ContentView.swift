c94a3742e0ba98b0afa41ccc2c008cab62197e9f
Taarna <ivana.rast@gmail.com>
2024-06-08 14:57:53 +0200

    Added model

--- 06 - Project 4/BetterRest/BetterRest/ContentView.swift
+++ 06 - Project 4/BetterRest/BetterRest/ContentView.swift
@@ -1,14 +1,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours",
+                    value: $sleepAmount,
+                    in: 4...12,
+                    step: 0.25
+            )
+            DatePicker("Please enter date", selection: $wakeUp, in: Date.now...)
+                .labelsHidden()
         }
         .padding()
+    }
+    
+    private func exampleDates() {
+        // create a second Date instance set to one day in seconds from now
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        // create a range from those two
+        let range = Date.now...tomorrow
+        
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? Date.now
+        
+        let componentsNow = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
