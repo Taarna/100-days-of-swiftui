d35742b29b0dfe3c67bf9c88453a73f542fc76af
Taarna <ivana.rast@gmail.com>
2024-06-09 15:06:13 +0200

    Day 27 changes

--- 06 - Project 4/BetterRest/BetterRest/ContentView.swift
+++ 06 - Project 4/BetterRest/BetterRest/ContentView.swift
@@ -1,36 +1,80 @@
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUpTime = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
-    var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours",
-                    value: $sleepAmount,
-                    in: 4...12,
-                    step: 0.25
-            )
-            DatePicker("Please enter date", selection: $wakeUp, in: Date.now...)
-                .labelsHidden()
-        }
-        .padding()
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
     }
     
-    private func exampleDates() {
-        // create a second Date instance set to one day in seconds from now
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        // create a range from those two
-        let range = Date.now...tomorrow
+    var body: some View {
+        NavigationStack {
+            Form {
+                VStack(alignment: .leading) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUpTime, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                VStack(alignment: .leading) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                VStack(alignment: .leading) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                }
+                Spacer()
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
+        }
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUpTime - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
         
-        var components = DateComponents()
-        components.hour = 8
-        components.minute = 0
-        let date = Calendar.current.date(from: components) ?? Date.now
-        
-        let componentsNow = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
+        showingAlert = true
     }
 }
 
