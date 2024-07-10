b6fa08a911da883cc970ec4242bf47524fc5a198
Taarna <ivana.rast@gmail.com>
2024-07-10 09:15:38 +0200

    Day 50

--- 14 - Project 10/CupcakeCorner/CupcakeCorner/Response.swift
+++
@@ -1,4 +0,0 @@
-
-struct Response: Codable {
-    var results: [Result]
-}
--- 14 - Project 10/CupcakeCorner/CupcakeCorner/Result.swift
+++
@@ -1,6 +0,0 @@
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
