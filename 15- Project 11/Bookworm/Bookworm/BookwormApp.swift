6001a45245d5b25b66db9be45bec48263556851e
Taarna <ivana.rast@gmail.com>
2024-07-12 16:49:48 +0200

    Day 53

---
+++ 15- Project 11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import SwiftData
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 15- Project 11/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    var body: some View {
+        NavigationStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
