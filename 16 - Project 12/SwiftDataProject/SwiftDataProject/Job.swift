0f4f87e34a91f5f84b4b0c1dac41a04423eba9ca
Taarna <ivana.rast@gmail.com>
2024-07-20 00:00:58 +0200

    Day 58 (without CloudKit)

---
+++ 16 - Project 12/SwiftDataProject/SwiftDataProject/Job.swift
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Job {
+    var name: String
+    var priority: Int
+    var owner: User?
+
+    init(name: String, priority: Int, owner: User? = nil) {
+        self.name = name
+        self.priority = priority
+        self.owner = owner
+    }
+}
---
+++ 16 - Project 12/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import SwiftData
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: User.self)
+    }
+}
