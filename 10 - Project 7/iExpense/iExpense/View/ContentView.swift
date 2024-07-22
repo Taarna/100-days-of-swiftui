796724297c2f113789036c5ab865b434ae9e3bbe
Taarna <ivana.rast@gmail.com>
2024-07-22 11:28:04 +0200

    Day 59 - Challenge 3

--- 10 - Project 7/iExpense/iExpense/View/ContentView.swift
+++ 10 - Project 7/iExpense/iExpense/View/ContentView.swift
@@ -6,40 +6,55 @@ import Observation
 import SwiftData
 import SwiftUI
 
+enum ExpenseFilter {
+    case all, privateItems, businessItems
+}
+
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var sortOrder = [
         SortDescriptor(\ExpenseItem.name)
     ]
+    @State private var filterOption: ExpenseFilter = .all
     @Query(sort: \ExpenseItem.name) var items: [ExpenseItem]
     
     @State private var isAddExpensePresented = false
     
+    @State private var showPrivate = true
+    @State private var showBusiness = true
+    
     var body: some View {
         NavigationStack {
-            ExpensesView(sortOrder: sortOrder)
-            .navigationTitle("iExpense")
-            .toolbar {
-                Button("Add Expense", systemImage: "plus") {
-                    isAddExpensePresented = true
-                }
-                Menu("Sort", systemImage: "arrow.up.arrow.down") {
-                    Picker("Sort", selection: $sortOrder) {
-                        Text("Sort by Name")
-                            .tag([
-                                SortDescriptor(\ExpenseItem.name)
-                            ])
-                        
-                        Text("Sort by Amount")
-                            .tag([
-                                SortDescriptor(\ExpenseItem.amount)
-                            ])
+            ExpensesView(sortOrder: sortOrder, filter: filterOption)
+                .navigationTitle("iExpense")
+                .toolbar {
+                    Button("Add Expense", systemImage: "plus") {
+                        isAddExpensePresented = true
+                    }
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Sort by Name")
+                                .tag([
+                                    SortDescriptor(\ExpenseItem.name)
+                                ])
+                            
+                            Text("Sort by Amount")
+                                .tag([
+                                    SortDescriptor(\ExpenseItem.amount)
+                                ])
+                        }
+                    }
+                    Menu("Filter", systemImage: "line.3.horizontal.decrease.circle") {
+                        Picker("Filter", selection: $filterOption) {
+                            Text("All").tag(ExpenseFilter.all)
+                            Text("Private").tag(ExpenseFilter.privateItems)
+                            Text("Business").tag(ExpenseFilter.businessItems)
+                        }
                     }
                 }
-            }
-            .navigationDestination(isPresented: $isAddExpensePresented, destination: {
-                AddView()
-            })
+                .navigationDestination(isPresented: $isAddExpensePresented, destination: {
+                    AddView()
+                })
         }
     }
     
@@ -55,8 +70,18 @@ struct ExpensesView: View {
     @Environment(\.modelContext) var modelContext
     @Query var items: [ExpenseItem]
     
-    init(sortOrder: [SortDescriptor<ExpenseItem>]) {
-        _items = Query(sort: sortOrder)
+    init(sortOrder: [SortDescriptor<ExpenseItem>], filter: ExpenseFilter) {
+        let predicate: Predicate<ExpenseItem>
+        switch filter {
+        case .all:
+            predicate = #Predicate<ExpenseItem> { _ in true }
+        case .privateItems:
+            predicate = #Predicate<ExpenseItem> { $0.isPrivate == true }
+        case .businessItems:
+            predicate = #Predicate<ExpenseItem> { $0.isPrivate == false }
+        }
+        
+        _items = Query(filter: predicate, sort: sortOrder)
     }
     
     var body: some View {
