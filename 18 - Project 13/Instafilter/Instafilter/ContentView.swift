c92cd11c55c5190807c94badb2ca90162e0d0730
Taarna <ivana.rast@gmail.com>
2024-08-02 14:07:15 +0200

    Day 65

--- 18 - Project 13/Instafilter/Instafilter/ContentView.swift
+++ 18 - Project 13/Instafilter/Instafilter/ContentView.swift
@@ -4,59 +4,77 @@
 
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
+    @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     
-    private var inputImage = UIImage(resource: .losinj)
-    private var context = CIContext()
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        ZStack {
-            Color.black.edgesIgnoringSafeArea(.all)
-            
-            VStack(spacing: 20) {
-                Text("Day 63 Completed! 🎉")
-                    .font(.title2)
-                    .foregroundColor(.white)
+        NavigationStack {
+            VStack {
+                Spacer()
                 
-                image?
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 300)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 2))
-                
-                Slider(value: $filterIntensity, in: 0...1)
-                    .onChange(of: filterIntensity) {
-                        loadImage()
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Import a photo to get started"))
                     }
-                    .tint(.white)
+                }
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // share the picture
+                }
             }
-            .padding()
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .onAppear(perform: loadImage)
     }
     
     func loadImage() {
-        applyFilter()
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
     }
     
-    func applyFilter() {
-        guard let beginImage = CIImage(image: inputImage) else { return }
-        
-        // Apply the Instant filter
-        let instantFilter = CIFilter.photoEffectTransfer()
-        instantFilter.inputImage = beginImage
-        
-        guard var instantOutput = instantFilter.outputImage else { return }
-        
-        guard let cgImage = context.createCGImage(instantOutput, from: instantOutput.extent) else { return }
-        
+    func changeFilter() {
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
         let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
