419af51d2f5492e48b20afb7d06095f236f08c5c
Taarna <ivana.rast@gmail.com>
2024-08-11 09:19:34 +0200

    Day 74

---
+++ 20 - Project 15/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
