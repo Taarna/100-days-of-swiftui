e78b85b0a4bac12cd00e7a0cf5c15cc0d01e9929
Taarna <ivana.rast@gmail.com>
2024-08-22 12:22:22 +0200

    Day 79

---
+++ 22 - Project 16/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
