777b21ba7406e799af9976b9f0332635eaf4bc48
Taarna <ivana.rast@gmail.com>
2024-08-24 15:19:55 +0200

    Day 81

--- 22 - Project 16/HotProspects/HotProspects/ContentView.swift
+++ 22 - Project 16/HotProspects/HotProspects/ContentView.swift
@@ -2,35 +2,23 @@
 // Copyright (c) 2024 Ivana Rast. All rights reserved.
 //
 
+import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
+    let possibleNumbers = 1...60
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.formatted()
+    }
     
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change Color")
-                .padding()
-                .contextMenu {
-                    Button("Red", systemImage: "checkmark.circle.fill") {
-                        backgroundColor = .red
-                    }
-                    
-                    Button("Green") {
-                        backgroundColor = .green
-                    }
-                    
-                    Button("Blue") {
-                        backgroundColor = .blue
-                    }
-                }
-        }
+        Text(results)
     }
 }
+
 #Preview {
     ContentView()
 }
