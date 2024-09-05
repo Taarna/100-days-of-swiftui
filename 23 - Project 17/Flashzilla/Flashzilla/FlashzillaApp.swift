a7415dc1775093bbffead9e19d3d2a35f9ce3b70
Taarna <ivana.rast@gmail.com>
2024-09-05 10:18:52 +0200

    Day 88

--- 23 - Project 17/Flashzilla/Flashzilla/FlashzillaApp.swift
+++ 23 - Project 17/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -8,7 +8,7 @@ import SwiftUI
 struct FlashzillaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ 23 - Project 17/Flashzilla/Flashzilla/Model/Card.swift
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
