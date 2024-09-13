fed783e9ff9e5935d9c3252ab75f2d94100ece63
Taarna <ivana.rast@gmail.com>
2024-09-13 14:44:41 +0200

    Day 94 - Challenge 1

--- 24 - Project 18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 24 - Project 18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -6,24 +6,25 @@ import SwiftUI
 
 struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
-    
+
     var body: some View {
-        
-        ScrollView(showsIndicators: false) {
-            VStack(spacing: 16) {
-                ForEach(1..<20) { num in
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.teal)
-                        .frame(height: 90)
-                        .visualEffect { content, proxy in
-                            content
-                                .hueRotation(Angle(degrees: proxy.frame(in: .global).origin.y) / 10)
-                        }
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .visualEffect { content, geometryProxy in
+                                content
+                                    .opacity(geometryProxy.frame(in: .global).origin.y / 200)
+                            }
+                    }
+                    .frame(height: 40)
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
-        
     }
 }
