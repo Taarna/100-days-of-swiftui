72c0083de99ca05ce54a3166f80f310f50f9a63b
Taarna <ivana.rast@gmail.com>
2024-09-14 15:02:38 +0200

    Days 96 and 97

---
+++ 26 - Project 19/SnowSeeker/SnowSeeker/ContentView.swift
@@ -0,0 +1,59 @@
+//
+// Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import SwiftUI
+
+@Observable
+class Player {
+    var name = "Anonymous"
+    var highScore = 0
+}
+
+struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    var body: some View {
+        NavigationSplitView {
+            List(resorts) { resort in
+                NavigationLink(value: resort) {
+                    HStack {
+                        Image(resort.country)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 25)
+                            .clipShape(
+                                .rect(cornerRadius: 5)
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+        } detail: {
+            WelcomeView()
+        }
+    }
+}
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker!")
+                .font(.largeTitle)
+
+            Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+                .foregroundStyle(.secondary)
+        }
+    }
+}
---
+++ 26 - Project 19/SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2024 Ivana Rast. All rights reserved.
+//
+
+import Foundation
+
+struct Resort: Codable, Hashable, Identifiable {
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+    
+//    static let example = (Bundle.main.decode("resorts.json") as [Resort])[0]
+    
+    var id: String
+    var name: String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+}
